60981d3129aa706f397aa552cb4220383b3de1d4
KulikovIK <ilyatrg@gmail.com>
2023-04-15 17:26:09 +0500

    lesson_2

--- core/main_app.py
+++ core/main_app.py
@@ -1,8 +1,21 @@
 from flask import Flask
 
+from .index.views import index
+from .user.views import users_app
+from .articles.views import articles_app
 
-app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "hello world"
+VIEWS = [
+    index,
+    users_app,
+    articles_app,
+]
+
+def make_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
+
+def register_blueprints(app: Flask):
+    for view in VIEWS:
+        app.register_blueprint(view)
--- run.py
+++ run.py
@@ -1,7 +1,8 @@
-from core.main_app import app
+from core.main_app import make_app
 
 
 if __name__ == '__main__':
+    app = make_app()
     app.run(
         host="127.0.0.1",
         port="9900",
