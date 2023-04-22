df8319c43589571ee1886ae733d2813fcb570b49
KulikovIK <ilyatrg@gmail.com>
2023-04-22 22:44:26 +0500

    DC.yaml SQL_D_URI correct

--- core/config.py
+++ core/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config:
     DEBUG = os.getenv("DEBUG")
     TESTING = os.getenv("TESTING")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
     SECRET_KEY = os.getenv("SECRET_KEY")
